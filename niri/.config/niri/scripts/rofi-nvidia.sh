bb544961ee55f53f3848801e6e8f6cf78a70d236
vuongngo-dev <vuongngo.dev@gmail.com>
2026-05-09 16:09:18 +0700

    ref: configure border and enviroment

--- niri/.config/niri/scripts/rofi-nvidia.sh
+++ niri/.config/niri/scripts/rofi-nvidia.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-rofi -show drun -theme "$HOME/.config/rofi/config.rasi" -run-command "switcherooctl launch {cmd}" -display-drun "🚀 NVIDIA"
+
+# Kiểm tra xem prime-run có tồn tại không để tránh lỗi
+if ! command -v prime-run &> /dev/null; then
+    notify-send "Lỗi" "Không tìm thấy lệnh prime-run. Vui lòng cài đặt driver NVIDIA."
+    exit 1
+fi
+
+rofi -show drun \
+    -theme "$HOME/.config/rofi/config.rasi" \
+    -run-list-command ". $HOME/.bashrc" \
+    -run-command "prime-run {cmd}" \
+    -display-drun "󰾲  GPU Mode" \
+    -drun-display-format "{name}"
