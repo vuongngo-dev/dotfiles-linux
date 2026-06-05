7e8dd0c80984cb1ff1c550e4b28d571aff6854e2
vuongngo-dev <vuongngo.dev@gmail.com>
2026-06-05 09:53:54 +0700

    ref: reload file struct

---
+++ niri/.config/niri/scripts/rofi_nvidia.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Kiểm tra xem prime-run có tồn tại không để tránh lỗi
+if ! command -v prime-run &> /dev/null; then
+    notify-send "Error" "Not found driver NVIDIA"
+    exit 1
+fi
+
+rofi -show drun \
+    -theme "$HOME/.config/rofi/config.rasi" \
+    -run-list-command ". $HOME/.bashrc" \
+    -run-command "prime-run {cmd}" \
+    -display-drun "󰾲  GPU" \
+    -drun-display-format "{name}"
